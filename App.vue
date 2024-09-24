a8f579e3191df8d0b2950fe97c3b49fa3352eef6
MONDERASDOR <140967226+MONDERASDOR@users.noreply.github.com>
2024-09-24 12:29:27 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Shopping List</h1>
+    <input v-model="newItem" placeholder="Add a new item" />
+    <button @click="addItem">Add</button>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }} <button @click="removeItem(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newItem: '',
+      items: [],
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.newItem.trim()) {
+        this.items.push(this.newItem);
+        this.newItem = '';
+      }
+    },
+    removeItem(index) {
+      this.items.splice(index, 1);
+    },
+  },
+};
+</script>
